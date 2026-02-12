a7a27dd0460276ca335ba00877269068bad44389
Vincenzo Ingenito <54367888+vincenzo-ingenito@users.noreply.github.com>
2026-02-12 22:10:39 +0100

    Update Application.java

--- src/main/java/it/voyage/ms/Application.java
+++ src/main/java/it/voyage/ms/Application.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
